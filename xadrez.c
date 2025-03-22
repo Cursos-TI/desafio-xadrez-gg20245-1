abf19ef9ba51f6238ce6f3ae679a41433b9e6dde
gg20245 <gg471092@gmail.com>
2025-03-22 15:00:15 +0000

    criei o movimento do bispo

--- xadrez.c
+++ xadrez.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
- int main(){
- // Mover a Torre 5 casas para a direita
-    for(int i=0; i < 5; i++) {
-        printf("Direita\n"); // Imprime a direção do movimento
-    }
+
+int main(){
+    int bispo;
+    do{
+        printf("Digite o número de casas que o bispo pode mover (1-5): \n");
+        scanf("%d", &bispo);
+    } while (bispo < 1 || bispo > 5);
+    
+    printf("O bispo pode se mover até %d casas.\n", bispo);
+
+    printf("Cima,Direita\n");
+
 
     return 0;
- }
+}
